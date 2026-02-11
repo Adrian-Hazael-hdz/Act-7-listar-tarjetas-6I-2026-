b2c4a68c881075aca640cac09ff9c0256b64b3ab
a.23308051281128 <a.23308051281128@cbtis128.edu.mx>
2026-02-11 20:33:29 +0000

    otra vez

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,114 @@
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppPChop());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppPChop extends StatelessWidget {
+  const AppPChop({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: LosProductos(),
+
     );
   }
-}
+}// fin clase AppPChop
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LosProductos extends StatelessWidget {
+  const LosProductos({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    appBar: AppBar(
+  // Título con estilo de texto en negro
+  title: const Text(
+    'Tienda de electronicos PChop',
+    style: TextStyle(color: Colors.black),
+  ),
+  // Color de fondo morado claro
+  centerTitle: true,
+  backgroundColor: const Color.fromARGB(255, 214, 142, 226), 
+  // Icono de computadora a la izquierda (leading)
+  leading: const Icon(
+    Icons.computer,
+    color: Colors.black,
+  ),
+  // Lista de acciones a la derecha
+  actions: [
+    IconButton(
+      icon: const Icon(Icons.phone, color: Colors.black),
+      onPressed: () {},
+    ),
+    IconButton(
+      icon: const Icon(Icons.shopping_cart, color: Colors.black),
+      onPressed: () {},
+    ),
+    IconButton(
+      icon: const Icon(Icons.memory, color: Colors.black), // Icono de chip
+      onPressed: () {},
+    ),
+  ],
+),
+body: ListView(
+  padding: const EdgeInsets.all(16.0),
+  children: [
+    // Tarjeta 1 - Laptop
+    _crearTarjeta(
+      'Laptop Gamer Pro', 
+      'Procesador de última generación', 
+      'https://raw.githubusercontent.com/Adrian-Hazael-hdz/Imagenes-para-flutter-IAM-6to-I-fecha-11-feb-026/refs/heads/main/producto1.png'
+    ),
+    // Tarjeta 2 - Smartphone
+    _crearTarjeta(
+      'Laptop lenovo ', 
+      'Grafica integrada', 
+      'https://raw.githubusercontent.com/Adrian-Hazael-hdz/Imagenes-para-flutter-IAM-6to-I-fecha-11-feb-026/refs/heads/main/producto2.png'
+    ),
+    // Tarjeta 3 - Audífonos
+    _crearTarjeta(
+      'Laptop de oficina', 
+      'Batería de 40 horas', 
+      'https://raw.githubusercontent.com/Adrian-Hazael-hdz/Imagenes-para-flutter-IAM-6to-I-fecha-11-feb-026/refs/heads/main/producto3.png'
+    ),
+    // Tarjeta 4 - Monitor
+    _crearTarjeta(
+      'Monitor 4K Curvo', 
+      '144Hz para gaming fluido', 
+      'https://raw.githubusercontent.com/Adrian-Hazael-hdz/Imagenes-para-flutter-IAM-6to-I-fecha-11-feb-026/refs/heads/main/producto4.png'
+    ),
+  ],
+),
+
+    );// fin de scaffold
   }
+}// fin clase LosProductos
+
+// Esta función va fuera del build, pero dentro de tu clase
+Widget _crearTarjeta(String titulo, String subtitulo, String urlImagen) {
+  return Card(
+    color: Colors.blue[50], // Azul claro
+    elevation: 5,           // Sombreado
+    margin: const EdgeInsets.only(bottom: 12),
+    child: ListTile(
+      // Imagen desde la red configurada mediante el parámetro urlImagen
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(urlImagen),
+      ),
+      title: Text(
+        titulo, 
+        style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold)
+      ),
+      subtitle: Text(
+        subtitulo, 
+        style: const TextStyle(color: Colors.black54)
+      ),
+      trailing: const Icon(
+        Icons.favorite_border, 
+        color: Colors.black
+      ),
+    ),
+  );
 }
